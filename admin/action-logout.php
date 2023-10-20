64a5e6a18cc2dd6fc15a7cf5d5cd3c3bd2e54fb7
jonathanChabert <jonathan.c@valnetinc.com>
2023-10-20 17:23:07 -0400

    removing staging in content

--- admin/action-logout.php
+++ admin/action-logout.php
@@ -1,1 +1,1 @@
-<?php	session_start();	mysqli_close();	session_destroy();    $websiteUrl = 'https://staging.chabertdesign.com/';	header('Location: '.$websiteUrl.'?m=12');	exit();?>
+<?php	session_start();	mysqli_close();	session_destroy();    $websiteUrl = 'https://www.chabertdesign.com/';	header('Location: '.$websiteUrl.'?m=12');	exit();?>
